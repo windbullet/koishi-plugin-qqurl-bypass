3a165c74ab814381fe4c89642d16df57fe0b0f61
windbullet <2536810643@qq.com>
2023-12-26 10:15:13 +0800

    提交文件

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import { Context, Schema } from 'koishi'
+import { before } from 'node:test'
 
 export const name = 'qqurl-bypass'
 
@@ -7,5 +8,7 @@ export interface Config {}
 export const Config: Schema<Config> = Schema.object({})
 
 export function apply(ctx: Context) {
-  // write your plugin here
+  ctx.on("before-send", (session) => {
+    session.content = session.content.replace(/\./g, "‎.")
+  })
 }

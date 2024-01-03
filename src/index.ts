7da25c8e040129c45899539c9b28c0158a732c3c
windbullet <2536810643@qq.com>
2024-01-03 16:59:52 +0800

    更新

--- src/index.ts
+++ src/index.ts
@@ -3,12 +3,34 @@ import { before } from 'node:test'
 
 export const name = 'qqurl-bypass'
 
-export interface Config {}
+export interface Config {
+  mode: 'unicode' | 'space' | 'fullStop'
+}
 
-export const Config: Schema<Config> = Schema.object({})
+export const Config: Schema<Config> = Schema.object({
+  mode: Schema.union([
+    Schema.const('unicode').description('点号前插入unicode字符（不可见所以无痕，但复制访问不方便）'),
+    Schema.const('space').description('点号前插入空格（复制访问相对来说方便些）'),
+    Schema.const('fullStop').description('点号替换为中文句号（可直接复制访问）')
+  ])
+    .description('绕过模式')
+    .required()
+    .role('radio')
 
-export function apply(ctx: Context) {
+})
+
+export function apply(ctx: Context, config: Config) {
   ctx.on("before-send", (session) => {
-    session.content = session.content.replace(/\./g, "‎.")
+    switch (config.mode) {
+      case 'unicode':
+        session.content = session.content.replace(/\./g, "‎.")
+        break
+      case 'space':
+        session.content = session.content.replace(/\./g, " .")
+        break
+      case 'fullStop':
+        session.content = session.content.replace(/\./g, "。")
+        break
+    }
   })
 }
